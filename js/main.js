b0dbe23f313426c4432f795cf76cdb404963698d
Даниил Полыгалов <cristalit42@gmail.com>
2025-08-31 19:25:31 +0600

    Август 2025

--- js/main.js
+++ js/main.js
@@ -44,7 +44,7 @@ $('.advantages__carousel').slick({
     $('body,html').animate({ scrollTop: top }, 800)
   });
 
-  $(".header__nav-item a, .footer__nav-link, .btn, .btn--black, .overlay__nav-link").on("click", function (e) {
+  $(".header__nav-link, .footer__nav-link, .header__main-btn, .create__btn-box a, .advantages__btn-box a, .btn--black, .overlay__nav-link, #footer__btn").on("click", function (e) {
     e.preventDefault()
     var id = $(this).attr('href'),
       top = $(id).offset().top - 50
@@ -56,4 +56,29 @@ $('.advantages__carousel').slick({
   //MENU START
 
   //MENU END
+
+  $(document).ready(function() {
+    $('.gallery__content-link').on('click', function(event) {
+        event.preventDefault();
+
+        var fancyboxGroup = $(this).data('fancybox');
+        var modalImages = $('.gallery__modal a[data-fancybox="' + fancyboxGroup + '"]').map(function() {
+            return {
+                src: $(this).attr('href'),
+                type: 'image'
+            };
+        }).get();
+
+        // Открываем галерею только с изображениями из модального окна
+        Fancybox.show(modalImages, {
+            loop: false,
+            // Другие настройки Fancybox
+        });
+    });
 });
+
+
+
+
+});
+
